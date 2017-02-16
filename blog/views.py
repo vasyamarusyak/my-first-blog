43abd30b0d96a42438a5e2c8645963e443fc02bd
vasyamarusyak <vasya_marusyak@i.ua>
2017-02-16 13:53:43 +0200

    Changed the HTML for the site.

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,6 @@
 from django.shortcuts import render
 
 # Create your views here.
+def post_list(request):
+    print(type (render(request, 'blog/post_list.html', {})))
+    return render(request, 'blog/post_list.html', {})
